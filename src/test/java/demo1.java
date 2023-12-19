e0a9d1a2d1227d2f5d01525a7bbb0e4d09a4bcdd
prabhat kumar <prabhat.kumar.24.2@mountblue.tech>
2023-12-19 17:03:43 +0530

    Initial commit

---
+++ src/test/java/demo1.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class demo1 {
+WebDriver driver;
+@Test(priority = 1)
+public  void test1() throws InterruptedException {
+    ChromeOptions options= new ChromeOptions();
+//        options.addArguments("--headless", "--disable-gpu", "--no-sandbox");
+    Map<String, String> prefs = new HashMap<>();
+//    prefs.put("download.default_directory");
+//    options.setExperimentalOption("prefs", prefs);
+    driver= new ChromeDriver(options);
+
+    driver.get("https://www.google.com");
+    Thread.sleep(1000);
+    driver.manage().window().maximize();
+    Thread.sleep(1000);
+
+}
+    @Test(priority = 2)
+    public  void test2(){
+      driver.findElement(By.xpath("//textarea[@id='APjFqb']")).sendKeys("jenkins");
+        driver.findElement(By.xpath("//textarea[@id='APjFqb']")).sendKeys(Keys.ENTER);
+    }
+
+
+}
